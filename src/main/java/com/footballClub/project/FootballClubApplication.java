03ed74fd858d63c0fcb78d9db5badb10a08ffb60
leedonghu <ldh930517@gmail.com>
2021-11-15 15:31:36 +0900

    first

---
+++ src/main/java/com/footballClub/project/FootballClubApplication.java
@@ -0,0 +1,13 @@
+package com.footballClub.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballClubApplication.class, args);
+	}
+
+}
